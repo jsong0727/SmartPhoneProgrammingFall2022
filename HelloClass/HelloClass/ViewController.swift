441972aef15e305c934556f88487591035a030f3
jsong0727 <songjiayin082@gmail.com>
2022-09-15 22:31:52 -0700

    hw1

---
+++ HelloClass/HelloClass/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  HelloClass
+//
+//  Created by Jiayin Song on 9/15/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
