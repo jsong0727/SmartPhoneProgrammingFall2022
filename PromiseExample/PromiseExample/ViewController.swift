f4ce0b66390cc0e39b3d37135b2b5a2688929671
jsong0727 <songjiayin082@gmail.com>
2022-11-19 11:42:01 -0800

    delete api key

--- PromiseExample/PromiseExample/ViewController.swift
+++ PromiseExample/PromiseExample/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func getStockValue(_ sender: Any) {
-        let apiKey = "e3274c22b2fc4cdcbae825ef3964bcb9"
+        let apiKey = ""
                 
         var url = "https://financialmodelingprep.com/api/v3/quote/"
         url += txtSymbol.text!
